de7eb6f21178908a6871917ae78f3d7ed82f323b
Alex Wong <Alex.Wong@icims.com>
2025-12-01 23:00:36 -0500

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [preact()],
+    base: './',
     server: {
         allowedHosts: ['desktop-csa7m2j.tail2ff47e.ts.net', 'localhost']
     }
